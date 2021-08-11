c375d9d3603ec2a76a0deda4b19c2df7b607b8ad
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2021-08-11 10:59:34 +0200

    Add Noticeable Newspage embed Widget on first page

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export const config = {
     noticeable: {
         // TODO: replace with your own values
-        iconWidgetId: 'RLBKpIlRIoZKqJTcJ5HL'
+        iconWidgetId: 'RLBKpIlRIoZKqJTcJ5HL',
+        newspageEmbedWidgetId: 'h8kcHbu3po7P9livB8KP'
     }
 }
