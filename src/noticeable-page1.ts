dccbe5e15eb4034dfe5c1207b20ee7ce0932bbb5
Laurent Pellegrino <laurent.pellegrino@gmail.com>
2021-08-11 10:59:34 +0200

    Add Noticeable Newspage embed Widget on first page

--- src/noticeable-page1.ts
+++ src/noticeable-page1.ts
@@ -5,12 +5,23 @@ import {
   LitElement,
   TemplateResult,
 } from 'lit-element';
+import {config} from './config';
 
 @customElement('noticeable-page1')
 export class NoticeablePage1 extends LitElement {
-  public static styles = css``;
+
+  public static styles = css`:host { width: 100% };`;
+
+  protected firstUpdated(): void {
+    noticeable.render('widget', config.noticeable.newspageEmbedWidgetId, {root: this.shadowRoot, selector: '#noticeable-newspage-embed'});
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    noticeable.destroy('widget', config.noticeable.newspageEmbedWidgetId);
+  }
 
   protected render(): TemplateResult {
-    return html`Page1`;
+    return html`<div id="noticeable-newspage-embed"></div>`;
   }
 }
